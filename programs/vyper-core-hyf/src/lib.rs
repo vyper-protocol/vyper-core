731cce152ab942b393b6f7a618c12b97e5e0a74d
dutch <thegreat.vanderlinde@protonmail.com>
2022-03-28 19:01:56 +0200

    suppress warning

--- programs/vyper-core-hyf/src/lib.rs
+++ programs/vyper-core-hyf/src/lib.rs
@@ -5,7 +5,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod vyper_core_hyf {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn initialize(_ctx: Context<Initialize>) -> ProgramResult {
         Ok(())
     }
 }
