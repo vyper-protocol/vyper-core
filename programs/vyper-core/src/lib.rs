658c7f6d74eae79c61ab144dc0166451cfca0e6e
dutch <giacomo@vyperprotocol.io>
2022-06-06 18:49:20 +0200

    introduced solana security txt

--- programs/vyper-core/src/lib.rs
+++ programs/vyper-core/src/lib.rs
@@ -6,6 +6,16 @@ pub mod utils;
 use anchor_lang::prelude::*;
 use instructions::*;
 
+use solana_security_txt::security_txt;
+
+security_txt! {
+    // Required fields
+    name: "vyper core",
+    project_url: "https://github.com/vyper-protocol/vyper-core",
+    contacts: "email:info@vyperprotocol.io,link:https://docs.vyperprotocol.io/security,discord:uKHfg58j",
+    policy: "https://github.com/vyper-protocol/blob/master/SECURITY.md"
+}
+
 declare_id!("mb9NrZKiC3ZYUutgGhXwwkAL6Jkvmu5WLDbxWRZ8L9U");
 
 #[program]
