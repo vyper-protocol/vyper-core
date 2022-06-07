eee34876dec4e982bf6fde8b240d8e52d3c613d2
dutch <giacomo@vyperprotocol.io>
2022-06-07 17:40:24 +0200

    rate mock plugin

--- programs/rate-mock/src/lib.rs
+++ programs/rate-mock/src/lib.rs
@@ -1,5 +1,4 @@
 use anchor_lang::prelude::*;
-use vyper_utils::math::{to_bps};
 
 declare_id!("9h7eHiqpPbj5Mw5AG59bFH7XDWfHYK52XMfJPbKVas2m");
 
@@ -8,17 +7,56 @@ pub mod rate_mock {
 
     use super::*;
 
-    pub fn fetch_rate(_ctx: Context<CpiReturnContext>) -> Result<u64> {
-        Ok(to_bps(0.33).into())
+    pub fn initialize(_ctx: Context<InitializeContext>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn set_random_fair_value(ctx: Context<RefreshRateContext>) -> Result<()> {
+
+        // random rate
+        let clock = Clock::get()?;
+        ctx.accounts.rate_data.fair_value = clock.unix_timestamp.checked_rem(10000).unwrap() as u64;
+        ctx.accounts.rate_data.refreshed_slot = clock.slot;
+
+        Ok(())
+    }
+
+    pub fn set_fair_value(ctx: Context<RefreshRateContext>, fair_value: u64) -> Result<()> {
+
+        let clock = Clock::get()?;
+        ctx.accounts.rate_data.fair_value = fair_value;
+        ctx.accounts.rate_data.refreshed_slot = clock.slot;
+
+        Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct CpiReturnContext<'info> {
-    pub account: Account<'info, CpiReturnAccount>,
+pub struct InitializeContext<'info> {
+    
+    /// Signer account
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(init, payer = signer, space = 1024)]
+    pub rate_data: Account<'info, RateState>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct RefreshRateContext<'info> {
+    /// Signer account
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    /// CHECK: 
+    #[account(mut)]
+    pub rate_data: Account<'info, RateState>,
 }
 
 #[account]
-pub struct CpiReturnAccount {
-    pub value: u64,
+pub struct RateState {
+    pub fair_value: u64,
+    pub refreshed_slot: u64,
 }
