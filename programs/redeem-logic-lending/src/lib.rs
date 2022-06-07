d208d2305d60e13f9582eb3e5df4f162dc7fd7f3
dutch <giacomo@vyperprotocol.io>
2022-06-07 22:08:24 +0200

    redeem logic lending program

---
+++ programs/redeem-logic-lending/src/lib.rs
@@ -0,0 +1,78 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Gc2ZKNuCpdNKhAzEGS2G9rBSiz4z8MULuC3M3t8EqdWA");
+
+#[program]
+pub mod redeem_logic_lending {
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<InitializeContext>, interest_split: [u64; 2]) -> Result<()> {
+        let redeem_logic_config = &mut ctx.accounts.redeem_logic_config;
+
+        redeem_logic_config.owner = ctx.accounts.owner.key();
+        redeem_logic_config.interest_split = interest_split;
+
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<UpdateContext>, interest_split: [u64; 2]) -> Result<()> {
+        let redeem_logic_config = &mut ctx.accounts.redeem_logic_config;
+
+        redeem_logic_config.owner = ctx.accounts.owner.key();
+        redeem_logic_config.interest_split = interest_split;
+
+        Ok(())
+    }
+
+    pub fn execute(_ctx: Context<ExecuteContext>, _old_tranche_fair_value: [u64; 2], _old_reserve_fair_value: u64, _new_reserve_fair_value: u64) -> Result<[u64; 2]> {
+
+        Ok([1993, 1993])
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeContext<'info> {
+    
+    /// Tranche config account, where all the parameters are saved
+    /// TODO size TBD
+    #[account(init, payer = payer, space = 8 + 16 + 32)]
+    pub redeem_logic_config: Box<Account<'info, RedeemLogicConfig>>, 
+
+    /// CHECK: Owner of the tranche config
+    #[account()]
+    pub owner: AccountInfo<'info>,
+    
+    /// Signer account
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateContext<'info> {
+
+    #[account(mut, has_one = owner)]
+    pub redeem_logic_config: Account<'info, RedeemLogicConfig>, 
+
+    /// CHECK: Owner of the tranche config
+    #[account()]
+    pub owner: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ExecuteContext<'info> {
+
+    #[account()]
+    pub redeem_logic_config: Account<'info, RedeemLogicConfig>, 
+
+    /// CHECK: Owner of the tranche config
+    #[account()]
+    pub signer: Signer<'info>,
+}
+
+#[account]
+pub struct RedeemLogicConfig {
+    pub interest_split: [u64; 2],
+    pub owner: Pubkey,
+}
---
+++ tests/redeem-logic-lending.ts
@@ -0,0 +1,120 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import { assert, expect } from "chai";
+import { RedeemLogicLending } from "../target/types/redeem_logic_lending";
+import { bn } from "./utils";
+
+describe.only("redeem_logic_lending", async () => {
+    const provider = anchor.AnchorProvider.env();
+
+    // Configure the client to use the local cluster.
+    anchor.setProvider(provider);
+    const program = anchor.workspace.RedeemLogicLending as Program<RedeemLogicLending>;
+
+    it("initialize", async () => {
+        const redeemLogicConfig = anchor.web3.Keypair.generate();
+
+        const interestSplit = [bn(5000), bn(10000)];
+        await program.methods
+            .initialize(interestSplit)
+            .accounts({
+                redeemLogicConfig: redeemLogicConfig.publicKey,
+                owner: provider.wallet.publicKey,
+                payer: provider.wallet.publicKey,
+            })
+            .signers([redeemLogicConfig])
+            .rpc();
+
+        const redeemLogicAccount = await program.account.redeemLogicConfig.fetch(redeemLogicConfig.publicKey);
+        expect(redeemLogicAccount.interestSplit.map((c) => c.toNumber())).to.eql(
+            interestSplit.map((c) => c.toNumber())
+        );
+        expect(redeemLogicAccount.owner.toBase58()).to.eq(provider.wallet.publicKey.toBase58());
+    });
+
+    it("update", async () => {
+        const redeemLogicConfig = anchor.web3.Keypair.generate();
+
+        const originalInterestSplit = [bn(5000), bn(10000)];
+        const newInterestSplit = [bn(6000), bn(10000)];
+        await program.methods
+            .initialize(originalInterestSplit)
+            .accounts({
+                redeemLogicConfig: redeemLogicConfig.publicKey,
+                owner: provider.wallet.publicKey,
+                payer: provider.wallet.publicKey,
+            })
+            .signers([redeemLogicConfig])
+            .rpc();
+        await program.methods
+            .update(newInterestSplit)
+            .accounts({
+                redeemLogicConfig: redeemLogicConfig.publicKey,
+                owner: provider.wallet.publicKey,
+            })
+            .rpc();
+        const redeemLogicAccount = await program.account.redeemLogicConfig.fetch(redeemLogicConfig.publicKey);
+        expect(redeemLogicAccount.interestSplit.map((c) => c.toNumber())).to.eql(
+            newInterestSplit.map((c) => c.toNumber())
+        );
+    });
+
+    it("reject non owner update", async () => {
+        const redeemLogicConfig = anchor.web3.Keypair.generate();
+
+        const originalInterestSplit = [bn(5000), bn(10000)];
+        const newInterestSplit = [bn(6000), bn(10000)];
+
+        await program.methods
+            .initialize(originalInterestSplit)
+            .accounts({
+                redeemLogicConfig: redeemLogicConfig.publicKey,
+                owner: provider.wallet.publicKey,
+                payer: provider.wallet.publicKey,
+            })
+            .signers([redeemLogicConfig])
+            .rpc();
+
+        try {
+            await program.methods
+                .update(newInterestSplit)
+                .accounts({
+                    redeemLogicConfig: redeemLogicConfig.publicKey,
+                    owner: anchor.web3.Keypair.generate().publicKey,
+                })
+                .rpc();
+            expect(false).to.be.true;
+        } catch (err) {
+            assert(true);
+        }
+    });
+
+    it("execute", async () => {
+        const redeemLogicConfig = anchor.web3.Keypair.generate();
+
+        const interestSplit = [bn(5000), bn(10000)];
+
+        await program.methods
+            .initialize(interestSplit)
+            .accounts({
+                redeemLogicConfig: redeemLogicConfig.publicKey,
+                owner: provider.wallet.publicKey,
+                payer: provider.wallet.publicKey,
+            })
+            .signers([redeemLogicConfig])
+            .rpc();
+
+        const old_tranche_fv = [bn(5000), bn(10000)];
+        const old_reserve_fv = bn(5000);
+        const new_reserve_fv = bn(5000);
+
+        const viewResult = await program.methods
+            .execute(old_tranche_fv, old_reserve_fv, new_reserve_fv)
+            .accounts({
+                redeemLogicConfig: redeemLogicConfig.publicKey,
+                signer: provider.wallet.publicKey,
+            })
+            .view();
+        expect((viewResult as anchor.BN[]).map((c) => c.toNumber())).to.be.eql([1993, 1993]);
+    });
+});
