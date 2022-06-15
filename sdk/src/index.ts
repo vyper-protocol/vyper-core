719f0fa036ac6a0be4ac643a6dd78cba8af128e7
Akshat mangal <mangalakshat01@gmail.com>
2022-06-15 16:11:01 +0530

    Commented the testing function call

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -2,16 +2,16 @@ export * from './Program'
 export * from './TrancheConfig'
 
 // for testing
-import { AnchorProvider } from '@project-serum/anchor';
-import { getVyperCoreProgram } from './Program';
-import { PublicKey } from "@solana/web3.js";
-import { TracheConfig } from './TrancheConfig';
-import * as dotenv from 'dotenv';
-dotenv.config();
+// import { AnchorProvider } from '@project-serum/anchor';
+// import { getVyperCoreProgram } from './Program';
+// import { PublicKey } from "@solana/web3.js";
+// import { TracheConfig } from './TrancheConfig';
+// import * as dotenv from 'dotenv';
+// dotenv.config();
 
-(async () => {
-    const provider = AnchorProvider.env();
-    const program = getVyperCoreProgram(provider);
-    const account = await TracheConfig.fetchTracheConfiguration(new PublicKey('EVeGAc8fA2EcwFpoVNdWLZSpxqJ38hBZtsjF5N3YaZuL'), program)
-    console.log(account);
-})()
+// (async () => {
+//     const provider = AnchorProvider.env();
+//     const program = getVyperCoreProgram(provider);
+//     const account = await TracheConfig.fetchTracheConfiguration(new PublicKey('EVeGAc8fA2EcwFpoVNdWLZSpxqJ38hBZtsjF5N3YaZuL'), program)
+//     console.log(account);
+// })()
