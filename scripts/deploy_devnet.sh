020a817bf356545ccc49c24ec4aad9f271521a4f
dutch <giacomo@vyperprotocol.io>
2022-07-08 12:42:07 +0200

    helper deploy script

---
+++ scripts/deploy_devnet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# vyper core
+anchor deploy -p vyper-core --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-core-program-authority/authority.json
+
+# rate mock plugin
+# anchor deploy -p rate-mock --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-core-program-authority/authority.json
+
+# rate mock plugin
+# anchor deploy -p redeem-logic-lending-fee --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-core-program-authority/authority.json
+
+# # # # # # # # # # # # # # # # # # 
+# RECOVERY
+# # # # # # # # # # # # # # # # # # 
+
+# solana-keygen recover -o ./ephemeral-kp.json prompt:// 
+# solana program deploy --buffer ./ephemeral-kp.json --upgrade-authority ~/Dev/VyperWallets/vyper-core-program-authority/authority.json -k ~/Dev/VyperWallets/vyper-core-program-authority/authority.json ./target/deploy/vyper_core.so
