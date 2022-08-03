bff0c3ffc55f0de11657407bb11de40ba9e1579f
dutch <giacomo@vyperprotocol.io>
2022-08-03 11:36:58 +0200

    fixed vyper-utils

--- libs/vyper-utils/src/lib.rs
+++ libs/vyper-utils/src/lib.rs
@@ -1,4 +1,3 @@
 pub mod constants;
-pub mod errors;
 pub mod rate_common;
 pub mod redeem_logic_common;
--- libs/vyper-utils/src/redeem_logic_common.rs
+++ libs/vyper-utils/src/redeem_logic_common.rs
@@ -1,10 +1,11 @@
 use anchor_lang::prelude::*;
+use rust_decimal::Decimal;
 
 #[derive(AnchorSerialize, AnchorDeserialize, Debug)]
 pub struct RedeemLogicExecuteInput {
     pub old_quantity: [u64; 2],
-    pub old_reserve_fair_value_bps: [u32; 10],
-    pub new_reserve_fair_value_bps: [u32; 10],
+    pub old_reserve_fair_value: [Decimal; 10],
+    pub new_reserve_fair_value: [Decimal; 10],
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Debug)]
