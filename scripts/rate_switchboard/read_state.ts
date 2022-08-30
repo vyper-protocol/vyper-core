49cde373df5e68e0cb2e74c211728a2f06cf2fa5
dutch <giacomo@vyperprotocol.io>
2022-08-30 12:32:35 +0200

    fixed script

--- scripts/rate_switchboard/read_state.ts
+++ scripts/rate_switchboard/read_state.ts
@@ -1,9 +1,10 @@
 import * as anchor from "@project-serum/anchor";
 import { Program, Wallet } from "@project-serum/anchor";
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { RustDecimalWrapper } from "@vyper-protocol/rust-decimal-wrapper";
 import { IDL } from "../../target/types/rate_switchboard";
 
-const PLUGIN_PROGRAM_ID = new PublicKey("3By5v1am74SMQUkM9va8iHgNLVEjGVMRmMSt9nDiuczZ");
+const PLUGIN_PROGRAM_ID = new PublicKey("2hGXiH1oEQwjCXRx8bNdHTi49ScZp7Mj2bxcjxtULKe1");
 const PLUGIN_STATE = new PublicKey("5iz3MJ8cnRcXmzBDgNmb65HPwfeiw4djUfEaLqKuBA41");
 
 const main = async () => {
@@ -16,8 +17,10 @@ const main = async () => {
     const program = new Program(IDL, PLUGIN_PROGRAM_ID, provider);
     const account = await program.account.rateState.fetch(PLUGIN_STATE);
 
-    // console.log("account: ", account);
-    console.log("fairValue: " + account.fairValue);
+    console.log("account: ", account);
+    account.fairValue.forEach((c, i) => {
+        console.log(`fairValue #${i}: ` + new RustDecimalWrapper(new Uint8Array(c)).toNumber());
+    });
     console.log("refreshedSlot: " + account.refreshedSlot.toNumber());
     console.log(
         "aggregators: " +
