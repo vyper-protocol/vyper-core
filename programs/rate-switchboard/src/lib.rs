378772ba6b2fe527fe0ad028556db93c57c78be1
dutch <giacomo@vyperprotocol.io>
2022-09-05 22:29:03 +0200

    new rate_switchboard program id

--- programs/rate-switchboard/src/lib.rs
+++ programs/rate-switchboard/src/lib.rs
@@ -5,7 +5,7 @@ use crate::errors::RateSwitchboardErrorCode;
 use anchor_lang::prelude::*;
 use rust_decimal::{prelude::FromPrimitive, Decimal};
 use switchboard_v2::{AggregatorAccountData, SWITCHBOARD_V2_DEVNET, SWITCHBOARD_V2_MAINNET};
-declare_id!("EMg9whXe9Yk1vdmEwBmEfnucRAaombxC4HW1LyRy1tWD");
+declare_id!("2hGXiH1oEQwjCXRx8bNdHTi49ScZp7Mj2bxcjxtULKe1");
 
 #[program]
 pub mod rate_switchboard {
