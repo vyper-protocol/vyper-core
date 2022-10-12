436c8efd8d830c5cb75be617a08686abeaee776c
dutch <giacomo@vyperprotocol.io>
2022-10-12 10:09:12 +0200

    rate pyth mainnet deploy

--- scripts/deploy_devnet.sh
+++ scripts/deploy_devnet.sh
@@ -9,23 +9,23 @@
 
 # rate switchboard
 # anchor deploy -p rate-switchboard --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
-anchor idl init --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/rate_switchboard.json 2hGXiH1oEQwjCXRx8bNdHTi49ScZp7Mj2bxcjxtULKe1
+# anchor idl init --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/rate_switchboard.json 2hGXiH1oEQwjCXRx8bNdHTi49ScZp7Mj2bxcjxtULKe1
 
 # rate pyth
-# anchor deploy -p rate-pyth --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
-anchor idl init --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/rate_pyth.json å
+anchor deploy -p rate-pyth --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
+anchor idl init --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/rate_pyth.json 3mxtC2cGVhHucUg4p58MVzVqUKLyiy1zWqRkRQdgUBPT
 
 # rate poolv2
 # anchor deploy -p rate-poolv2 --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
-anchor idl init --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/rate_poolv2.json 5Vm2YZK3SeGbXbtQpKVByP9EvYy78ahnjFXKkf9B3yzW
+# anchor idl init --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/rate_poolv2.json 5Vm2YZK3SeGbXbtQpKVByP9EvYy78ahnjFXKkf9B3yzW
 
 # redeem logic fee plugin
 # anchor deploy -p redeem-logic-lending-fee --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
 
 # redeem logic farming
 # anchor deploy -p redeem-logic-farming --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
-anchor idl init --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/redeem_logic_farming.json Fd87TGcYmWs1Gfa7XXZycJwt9kXjRs8axMtxCWtCmowN
-anchor idl upgrade --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/redeem_logic_farming.json Fd87TGcYmWs1Gfa7XXZycJwt9kXjRs8axMtxCWtCmowN
+# anchor idl init --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/redeem_logic_farming.json Fd87TGcYmWs1Gfa7XXZycJwt9kXjRs8axMtxCWtCmowN
+# anchor idl upgrade --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json -f ./target/idl/redeem_logic_farming.json Fd87TGcYmWs1Gfa7XXZycJwt9kXjRs8axMtxCWtCmowN
 
 # redeem logic vanilla option
 # anchor deploy -p redeem-logic-vanilla-option --provider.cluster d --provider.wallet ~/Dev/VyperWallets/vyper-program-authority/authority.json
@@ -39,4 +39,4 @@ anchor idl upgrade --provider.cluster m --provider.wallet ~/Dev/VyperWallets/vyp
 # # # # # # # # # # # # # # # # # # 
 
 # solana-keygen recover -o ./ephemeral-kp.json prompt:// 
-# solana program deploy --buffer ./ephemeral-kp.json --upgrade-authority ~/Dev/VyperWallets/vyper-program-authority/authority.json -k ~/Dev/VyperWallets/vyper-program-authority/authority.json ./target/deploy/redeem_logic_farming.so
+# solana program deploy --buffer ./ephemeral-kp.json --upgrade-authority ~/Dev/VyperWallets/vyper-program-authority/authority.json -k ~/Dev/VyperWallets/vyper-program-authority/authority.json ./target/deploy/rate_pyth.so
