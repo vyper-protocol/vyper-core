51a3815cb22c2fdba947c8e5ed012d1e622b132e
dutch <giacomo@vyperprotocol.io>
2022-10-30 18:01:29 +0100

    redeem logic digital security.txt

--- programs/redeem-logic-digital/src/lib.rs
+++ programs/redeem-logic-digital/src/lib.rs
@@ -10,6 +10,15 @@ use anchor_lang::prelude::*;
 use rust_decimal::prelude::*;
 use vyper_utils::redeem_logic_common::RedeemLogicErrors;
 
+solana_security_txt::security_txt! {
+    name: "Redeem Logic Digital | Vyper Core",
+    project_url: "https://vyperprotocol.io",
+    contacts: "email:info@vyperprotocol.io,link:https://docs.vyperprotocol.io/",
+    policy: "https://github.com/vyper-protocol/vyper-core/blob/master/SECURITY.md",
+    preferred_languages: "en",
+    source_code: "https://github.com/vyper-protocol/vyper-core/tree/main/programs/redeem-logic-digital"
+}
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
